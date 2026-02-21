19d68f5af7dc01b0931e30e41eca7c8be301b1dc
AYAN BHATTACHARJEE <bhattacharjeeayan93@gmail.com>
2026-02-21 18:15:48 +0530

    js fundamentals

---
+++ Saswat/Javascript/index.js
@@ -0,0 +1,172 @@
+// // console.log("hello world");
+// // console.log("second");
+
+
+// //variables
+// // var let const
+
+// // age = 22
+// // console.log(age)
+
+// // age = "hello"
+
+// //var
+// //Props - 1) redeclaration
+//         // 2) reassignment
+//         // 3) global scope
+           // 4) funtional scope
+// // var name = "Saswat"
+// // if(true){
+// //     var age = 16
+// // }
+// // console.log(age)
+
+// // var name      Step1: declaration
+// // name = "saswat"      //step2: initialization
+
+
+// // var name = "Saswat":
+// var name 
+// name = "Saswat "
+// name = "Ayan "
+
+// var name
+// var name
+// var name
+// var name
+// var name
+// var name
+// var name
+// var name
+// var name
+// var name 
+
+// var age = 20
+// age = 50 //assignment 
+// age = 60 //assignement 
+// age = "Sixty"
+
+
+
+// var name = 'Saswat';
+
+// function getName(){
+//     var name = "Saswat"
+// }
+// console.log(name)
+
+
+// {
+//     var name = "Saswat"
+    
+// }
+// console.log(name)
+
+
+// if(true){
+//     var name = "Saswat"
+// }
+
+// console.log(name)
+
+
+//let  
+//Props : - 1) No redeclaration
+// let name;
+// let name;
+
+//2 re assignment 
+// let coll = "ITER"
+// console.log(coll)
+// coll = "ITER, BBSR"
+// console.log(coll)
+// coll = 56 
+// console.log(coll)
+
+
+//scoping 
+//1) global scope
+//2) block scope 
+// let name = "Saswat"
+
+// {
+//     let name = "Saswat"
+//     // console.log(name)
+// }
+// console.log(name)
+
+// if(true){
+//     let name = "Ayan"
+// }
+// console.log(name)
+
+// function getColl(){
+//     let coll = "ITER"
+// }
+
+// console.log(coll)
+
+
+//const 
+// const age  = 10; 
+// let name;
+// var coll;
+//no resassignment 
+// age = 60
+
+// const coll = "ITER"
+// coll = "IIT"
+
+// {
+//     const age = 50
+// }
+
+// console.log(age)
+
+// if(true){
+//     const coll = "ITER"
+// }
+
+// console.log(coll)
+
+
+//Hoisting
+
+
+// console.log(a)
+
+// // var a;
+// const a = 10
+
+// console.log(name)
+// var name = "Saswat"
+// console.log(name)
+// let name = "Saswat"
+
+// console.log(name)
+// {
+//     console.log(name)
+//     let name = "Ayan"
+// }
+
+
+{
+    let name = "Saswat";
+    console.log(name)
+    {
+
+
+
+        
+        console.log(name);
+        let name = "Saswat"
+        console.log(name)
+    }
+}
+
+
+
+
+
+
+
