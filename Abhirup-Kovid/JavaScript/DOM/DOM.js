0c321106968eb8867490a06b77af63887b4d84b7
Abhirup Kovid <abhirupkovid@gmail.com>
2026-03-06 18:20:18 +0530

    DOM Manupulation

---
+++ Abhirup-Kovid/JavaScript/DOM/DOM.js
@@ -0,0 +1,69 @@
+// <!-- how the browser works
+
+// 1. you write html code 
+// 2. browser fetches the html file
+// 3. browser parses the html 
+// 4.browser creates a tree of nodes in memory 
+// 5. browser renders the page bases on the dom+css
+// 6. javascript csn now access and modify the dom
+// 7. any changes to the dom browser re renders the affected part -->
+
+// <!-- <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Document</title>
+// </head>
+// <body>
+//     <h1 id="main1">DOM EXAMPLE</h1>
+//     <div>
+//         <img src="">
+//     </div>
+// </body>
+// <script src="DOM.js">
+
+// </script>
+// </html> -->
+// <!--
+// document.getElementById('main1')
+// let v=document.ElementById('main1')
+// console.log(v.innertext)
+// v.innertext=Hi this changes h1 to hi
+
+// //getElementsById return element or undefined
+
+
+// </head>
+// -->
+
+
+
+// let element=document.getElementsByClassName("mera class")
+// console.log(element)
+// console.log(element[0])
+// //getElementByClassName 
+// // return type html collection
+
+// let element=document.getElementsByClassName("mera-class")
+// for(let i=0; i<element.length; i++){
+//     console.log(element[i].innerText)
+// }
+
+// let ele=document.querySelector("class1")
+// console.log(ele)
+
+
+// let ele = document.querySelector(".m-2")
+// console.log(ele)
+
+let cont=document.getElementById('div content')
+cont.style.backgroundColor='yellow'
+cont.style.margin='20px'
+cont.style.padding='20px'
+cont.style.height='90px'
+cont.style.border='10px solid black'
+cont.style.display='flex'
+cont.style.justifyContent='center'
+cont.style.fontSize='24px'
+cont.style.borderRadius='50%'
