2e21bf63a073a6569ebbe0562e768c1ac94a971a
Max Rex <trex.eternatus@gmail.com>
2026-03-07 17:24:24 +0530

    First DOM Class

---
+++ Arko-Roy-Chowdhury/DOM/Script.js
@@ -0,0 +1,13 @@
+// let ele = document.getElementById("First");
+// console.log(ele);
+
+// ele.innerText = "Hello World, Welcome to the DOM Class!";
+
+// ele.style.color = "blue";
+// ele.style.fontSize = "40px";
+// ele.style.textAlign = "center";
+
+let element = document.getElementsByClassName("Second");
+console.log(element);
+console.log(element[0]);
+console.log(typeof element);
