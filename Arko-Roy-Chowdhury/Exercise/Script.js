01fb1601017fa8842616432ff789cdf6b5ea7516
Max Rex <trex.eternatus@gmail.com>
2026-03-09 18:14:30 +0530

    Add exercise on changing color using js

---
+++ Arko-Roy-Chowdhury/Exercise/Script.js
@@ -0,0 +1,36 @@
+// const butr = document.querySelector("#red");
+// function redcolor(){
+//     const ele = document.querySelector("#one");
+//     ele.className = 'red';
+// }
+// butr.onclick = redcolor;
+
+
+
+// const butb = document.querySelector("#blue");
+// function bluecolor(){
+//     const ele = document.querySelector("#two");
+//     ele.className = 'blue';
+// }
+// butb.onclick = bluecolor;
+
+
+
+// const buto = document.querySelector("#orange");
+// function orangecolor(){
+//     const ele = document.querySelector("#four");
+//     ele.className = 'orange';
+// }
+// buto.onclick = orangecolor;
+
+
+
+// const butg = document.querySelector("#green");
+// function greencolor(){
+//     const ele = document.querySelector("#three");
+//     ele.className = 'green';
+// }
+// butg.onclick = greencolor;
+
+const butr = document.querySelector("#red");
+butr.addEventListener("click", redcolor);
