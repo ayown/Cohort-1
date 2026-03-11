f73d61ec981ab3fe3063be012dbca73bd5e1a710
sumanyu nayak <sumanyunayak@gmail.com>
2026-03-11 21:16:47 +0530

    FORM VALIDATION

--- Sumanyu-Nayak/Javascript/DOM.js
+++ Sumanyu-Nayak/Javascript/DOM.js
@@ -55,13 +55,57 @@
     //     element.className="red";
     // }
     // but.onclick=redcolor;
-    let colors=["red","green","blue","orange"]
-    function colorise(){
-        const element=document.querySelectorAll(".num");
-        colors.reverse();
-        for (let i=0;i<element.length;i++){
-            element[i].id=colors[i];
-        }
+    // let colors=["red","green","blue","orange"]
+    // function colorise(){
+    //     const element=document.querySelectorAll(".num");
+    //     colors.reverse();
+    //     for (let i=0;i<element.length;i++){
+    //         element[i].id=colors[i];
+    //     }
+    // }
+    // let but=document.querySelector("#but")
+    // but.addEventListener("Click",colorise)
+
+//setting a button
+// let div=document.getElementById("div-container")
+// const btn=document.createElement("button")
+// btn.textContent="Click Me"
+// div.appendChild(btn) // appendChild is a method that allows you to add a new child element to a parent element in the DOM. When you call div.appendChild(btn), it adds the button element (btn) as a child of the div element (container). This means that the button will be displayed within the container on the webpage.
+
+//Event Handling
+// const btn=document.getElementById("btn")
+
+//onclick with button
+// btn.onclick=function(){
+//     alert("Button Clicked")
+// }
+
+//addEventListener with all elements
+// btn.addEventListener("mouseover",function(){//mouse over 
+//     console.log("Mouse Over");
+//     alert("Mouse Over")
+//     this.style.backgroundColor="red"
+// })
+
+//FORM MANIPULATION/VALIDATION
+const form=document.getElementById("registrationForm")
+form.addEventListener("submit",function(event){
+    event.preventDefault() // page reload prevented
+
+    //Values Reading
+    const username=document.getElementById("username").value.trim()//trim removes the leading and trailing whitespaces
+    const email=document.getElementById("email").value.trim()
+    console.log("Username:", username)
+    console.log("Email:", email)
+
+    //Form Validation
+    if(username==="" || email===""){
+        alert("Please fill in all fields")
     }
-    const but=document.querySelector("#red")
-    but.addEventListener("click",colorise)
+    if(username.length<3){
+        alert("Username must be at least 3 characters long")
+        this.style.border="2px solid red"// this refers to the form element might be a div or input field or any form
+        
+    }
+})
+
